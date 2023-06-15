ae8061f39f29e8c83f75aabc1851e3c1999b3f20
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-06-15 11:41:08 +0000

    improve notepad design

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,19 +5,19 @@ const Home = () => {
 	const [ inputValue, setInputValue ] = useState("");
 	return (
 		<div className="container">
-			<h1>My Todos</h1>
+			<h1>My Todos <i class="fa-solid fa-pencil"></i></h1>
 			<ul>
 				<li><input 
 					type="text" 
 					onChange={(e) => setInputValue(e.target.value)}
 					value={inputValue}
-					placeholder="What do you need to do?" />
+					placeholder="What needs to be done?" />
 				</li>
 				<li>Make the bed <i class="fa-solid fa-trash-can"></i></li>
 				<li>Walk the dog <i class="fa-solid fa-trash-can"></i></li>
 				<li>Pay taxes <i class="fa-solid fa-trash-can"></i></li>
 			</ul>
-			<div>23 tasks</div>
+			<div><p>23 tasks</p></div>
 		</div>
 	);
 };
